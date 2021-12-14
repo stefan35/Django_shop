0a701ce9461ea8e92ecab24b4804c1104074c71f
Štefan <32110794+stefan35@users.noreply.github.com>
2021-12-14 16:05:41 +0100

    file update

--- shop/settings.py
+++ shop/settings.py
@@ -5,17 +5,10 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yl_d!!+6)(76vlhw24g7=@n5-#504ofkrn(5afehy9$#_jk=*)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
